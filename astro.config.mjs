bfc6f6500636435104716ea08cdea8d78e8a78d7
Assane GNING <gningaseto@hotmail.fr>
2025-01-06 14:59:46 +0100

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ASSANEASETO.github.io",
+  base: "/CvAssaneGning",
+  trailingSlash: "always",
   integrations: [tailwind()],
 });
